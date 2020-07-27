a1777dac583cb40fe4915137c5ecd706a042273c
LyricZhao <lyricz@yeah.net>
2020-07-27 10:43:00 +0800

    [Auto-deploy] Build 2020-07-27 10:07:00

--- deploy.sh
+++ deploy.sh
@@ -5,30 +5,34 @@ DEPLOY_DIR=../rCore-Tutorial-deploy/
 CURRENT_DIR=$(pwd)
 DEPLOY_DIR=$CURRENT_DIR/$DEPLOY_DIR
 
+# 判断是否需要重新 clone 仓库
 if [ -d "$DEPLOY_DIR" ]; then
     echo "$DEPLOY_DIR exists, resetting to remote master ..."
-    # Reset to master
     cd $DEPLOY_DIR
 else
     echo "$DEPLOY_DIR doesn't exist, cloning from remote ..."
     mkdir -p $DEPLOY_DIR
     cd $DEPLOY_DIR
     git init
+    # 该脚本将强制通过 SSH 协议和 GitHub 通信
     git remote add origin git@github.com:rcore-os/rCore-Tutorial-deploy.git
 fi
+
+# 重置到 remote master
 git fetch origin
 git reset --hard origin/master
 
-# Build and copy
+# 构建 GitBook 并复制
 cd $CURRENT_DIR
 gitbook build
 cp -r _book/* $DEPLOY_DIR
 cd $DEPLOY_DIR
 
-# Commit and push
+# Commit 并 push
 CURRENT_TIME=$(date +"%Y-%m-%d %H:%m:%S")
 git add *
 git commit -m "[Auto-deploy] Build $CURRENT_TIME"
 git push origin master
 
+# 返回当前目录
 cd $CURRENT_DIR
