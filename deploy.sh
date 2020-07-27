74c40b1fc2f7c525c74a18f0f8162234bdda49f0
LyricZhao <lyricz@yeah.net>
2020-07-27 10:32:30 +0800

    [Auto-deploy] Build 2020-07-27 10:07:30

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
-# 下面的 DEPLOY_DIR 目录需要关联到 https://github.com/rcore-os/rCore-Tutorial-deploy 远程仓库
-# 随后可以通过 https://rcore-os.github.io/rCore-Tutorial-deploy 来访问
+# 下面的 DEPLOY_DIR 目录将关联到 https://github.com/rcore-os/rCore-Tutorial-deploy 远程仓库
+# 部署后可以通过 https://rcore-os.github.io/rCore-Tutorial-deploy 来访问
 DEPLOY_DIR=../rCore-Tutorial-deploy/
 CURRENT_DIR=$(pwd)
 DEPLOY_DIR=$CURRENT_DIR/$DEPLOY_DIR
 
-# Reset to master
-cd $DEPLOY_DIR
+if [ -d "$DEPLOY_DIR" ]; then
+    echo "$DEPLOY_DIR exists, resetting to remote master ..."
+    # Reset to master
+    cd $DEPLOY_DIR
+else
+    echo "$DEPLOY_DIR doesn't exist, cloning from remote ..."
+    mkdir -p $DEPLOY_DIR
+    cd $DEPLOY_DIR
+    git init
+    git remote add origin git@github.com:rcore-os/rCore-Tutorial-deploy.git
+fi
 git fetch origin
 git reset --hard origin/master
 
