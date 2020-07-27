9a55d0251b9a6f5614e9f20280e3bf022eb34b1e
LyricZhao <lyricz@yeah.net>
2020-07-27 10:25:16 +0800

    [Auto-deploy] Build 2020-07-27 10:07:16

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,24 @@
 # 下面的 DEPLOY_DIR 目录需要关联到 https://github.com/rcore-os/rCore-Tutorial-deploy 远程仓库
 # 随后可以通过 https://rcore-os.github.io/rCore-Tutorial-deploy 来访问
 DEPLOY_DIR=../rCore-Tutorial-deploy/
+CURRENT_DIR=$(pwd)
+DEPLOY_DIR=$CURRENT_DIR/$DEPLOY_DIR
+
+# Reset to master
+cd $DEPLOY_DIR
+git fetch origin
+git reset --hard origin/master
 
 # Build and copy
+cd $CURRENT_DIR
 gitbook build
 cp -r _book/* $DEPLOY_DIR
-cd $DEPLOY_DIR || exit
+cd $DEPLOY_DIR
 
-CURRENT_TIME=$(date +"%Y-%m-%d %H:%m:%S")
 # Commit and push
+CURRENT_TIME=$(date +"%Y-%m-%d %H:%m:%S")
 git add *
 git commit -m "[Auto-deploy] Build $CURRENT_TIME"
 git push origin master
 
-cd - || exit
+cd $CURRENT_DIR
